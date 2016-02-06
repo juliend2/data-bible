05000f86916fdc3cf0f678c9b4c58a53005f9918
Julien Desrosiers <juliend2@gmail.com>
2016-02-06 11:32:03 -0500

    styles for word cloud

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+
+  def word_cloud(words)
+    html = ''
+    html += "<ul class='word-cloud'>"
+    higher_bound = words.first.last
+    lower_bound = words.last.last
+    range = higher_bound - lower_bound
+    x_small = range * 0.166666
+    small = range * 0.3333333
+    medium = range * 0.5
+    large = range * 0.666666
+    x_large = range * 0.833333
+    xx_large = range * 1
+    words.sort_by {|key, value| key }.each do |word, count|
+      size_class = case count
+                   when 0...x_small then 'x-small'
+                   when x_small...small then 'small'
+                   when small...medium then 'medium'
+                   when medium...large then 'large'
+                   when large...x_large then 'x-large'
+                   else 'xx-large'
+                   end
+      html += "<li class='#{size_class}'>" + word + "</li>"
+    end
+    html += "</ul>"
+    html
+  end
+
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -10,7 +10,7 @@ class Book < ActiveRecord::Base
       à après avant avec chez contre dans de depuis derrière dès devant en entre envers malgré
       par parmis pendant pour près sans selon sous sur vers voici voilà
       je tu il elle nous vous ils elles
-      que qu se ne pas du de et au tout
+      que qu se ne pas du de et au tout quil quils
       un une des
       son sa ses
       mon ma mes
@@ -19,26 +19,30 @@ class Book < ActiveRecord::Base
       toi ce où lui dit qui est moi toi était avait
       tous tout toute toutes
       donc ainsi même suis eux aux ou ni afin
-      cela cet cette ces ceux celles
-      plus puis encore sera où cest mais deux cette quand ans comme
-      ai eut commit prit appela donna fait fit fut vit ont leva faire mit dis avez dirent avoir été jai
-      le me on te car si ta là alors aussi as jusqu parce aujourd hui
+      cela cet cette ces ceux celles celui
+      plus puis encore sera où cest mais deux cette quand ans comme dont pourquoi répondit quant
+      ai eut commit prit appela donna fait fit fut vis vit ont leva fais faire mit dis avez dirent être avoir été jai aura donne sont soit avons avaient feras fera peut sais
+      le me on te car si ta là alors aussi as jusqu parce aujourd hui fois lorsque
+      000 pause
     ] + (1..9).to_a + ('a'..'z').to_a + [""]).flatten.map(&:to_s).uniq
-    version = Version.find_by_slug('SG21')
-    vv = VerseVersion.find_by_sql <<-EOSQL
-      SELECT  verse_versions.content
-      FROM    verse_versions
-        JOIN  verses ON verses.id = verse_versions.verse_id
-        JOIN  chapters ON chapters.id = verses.chapter_id
-        JOIN  books ON books.id = chapters.book_id
-      WHERE   books.id = #{id}
-        AND   verse_versions.version_id = #{version.id}
-    EOSQL
+    vv = Rails.cache.fetch("#{cache_key}/verse_content", expires_in: 12.hours) {
+      version = Version.find_by_slug('SG21')
+      VerseVersion.find_by_sql(<<-EOSQL)
+        SELECT  verse_versions.content
+        FROM    verse_versions
+          JOIN  verses ON verses.id = verse_versions.verse_id
+          JOIN  chapters ON chapters.id = verses.chapter_id
+          JOIN  books ON books.id = chapters.book_id
+        WHERE   books.id = #{id}
+          AND   verse_versions.version_id = #{version.id}
+      EOSQL
+    }
+    
 
     # tokenize
     words = vv.
       map {|v| v.content.split(/ /) }.flatten.
-      map {|word| word.split(/['-]/) }.flatten.
+      map {|word| word.split(/[’'-]/) }.flatten.
       map {|word| word.gsub(/[^[:word:]\s]/, '') }.map(&:downcase)
 
     # exclude some words
