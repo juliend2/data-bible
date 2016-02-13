ec27711ea5f575f10d617b5517276f786f1436ce
Julien Desrosiers <juliend2@gmail.com>
2016-02-13 13:59:48 -0500

    translate the app's UI in french

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
   def current_version
     if session[:current_version_id]
       @current_version ||= session[:current_version_id] && Version.find(session[:current_version_id])
