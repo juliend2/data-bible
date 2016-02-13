dd6c97bcfda5b4cfdd8d5a1725355595e49ef29d
Julien Desrosiers <juliend2@gmail.com>
2016-02-13 14:10:10 -0500

    Fix language toggle

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,14 @@ class ApplicationController < ActionController::Base
   def set_locale
     if params[:locale]
       session[:current_locale] = params[:locale]
+      I18n.locale = current_locale
     end
-    I18n.locale = session[:current_locale] || I18n.default_locale
   end
+
+  def current_locale
+    session[:current_locale] || I18n.default_locale
+  end
+  helper_method :current_locale
 
   def current_version
     if session[:current_version_id]
