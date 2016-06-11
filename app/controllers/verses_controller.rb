0aef9c9870dd786ed8e1c3a60dff5acce45f5926
Julien Desrosiers <juliend2@gmail.com>
2016-06-10 20:22:24 -0400

    Search in a single book at a time

--- app/controllers/verses_controller.rb
+++ app/controllers/verses_controller.rb
@@ -1,8 +1,23 @@
 class VersesController < ApplicationController
   def search
     @query = params[:query]
+    @books = Book.all
+    @book_id = params[:book_id]
     if @query
-      @verse_versions = VerseVersion.where("content LIKE '%#{@query}%' AND version_id = #{current_version.id}").includes(:verse)
+      if @book_id && @book_id.strip != ''
+        @verse_versions = VerseVersion.find_by_sql [<<-EOSQL, @book_id]
+          SELECT  verse_versions.*
+          FROM    verse_versions
+            JOIN  verses ON verses.id = verse_versions.verse_id
+            JOIN  chapters ON chapters.id = verses.chapter_id
+            JOIN  books ON books.id = chapters.book_id
+          WHERE   verse_versions.content LIKE '%#{@query}%'
+            AND   verse_versions.version_id = #{current_version.id}
+            AND   books.id = ?
+        EOSQL
+      else
+        @verse_versions = VerseVersion.where("content LIKE '%#{@query}%' AND version_id = #{current_version.id}").includes(:verse)
+      end
     else
       @verse_versions = []
     end
